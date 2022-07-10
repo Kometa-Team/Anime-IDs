f7ef6dc553b2e225b2ffa9931c5eddc3f66d13a7
meisnate12 <meisnate12@gmail.com>
2022-07-10 14:36:18 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Plex-Meta-Manager-Anime-IDs
-Anime ID Conversions for Plex Meta Manager
+# Plex Meta Manager Anime IDs
+Last generated at: July 10, 2022 06:34 PM
+
+This is a list for mapping MyAnimeList IDs and AniList IDs to AniDb IDs and then to TVDb IDs or IMDb IDs for us with [Plex-Meta-Manager](https://github.com/meisnate12/Plex-Meta-Manager).
+
+This list is generated daily at 12:00 AM UTC 
+
+## Lists
+The two lists used to make pmm_anime_ids.json are below. If something is incorrect it likely needs to be fixed in one of those locations.
+- [Anime-Lists/anime-lists](https://github.com/Anime-Lists/anime-lists/) for converting AniDb IDs to TVDb IDs or IMDb IDs
+- [manami-project/anime-offline-database](https://github.com/manami-project/anime-offline-database/) for converting MyAnimeList IDs and AniList IDs to AniDb IDs
+
+If they cannot be fixed at that location you can submit a PR to this repo and edit the pmm_edits.json file. Each entry is defined as follows:
+
+```json
+{
+  "1": {
+    "tvdb_id": 2,
+    "mal_id": 3,
+    "anilist_id": 4,
+    "imdb_id": "tt5"
+  }
+}
+```
---
+++ update_anime_ids.py
@@ -0,0 +1,69 @@
+import json, requests
+from datetime import datetime
+from lxml import html
+
+AniDBIDs = html.fromstring(requests.get("https://raw.githubusercontent.com/Anime-Lists/anime-lists/master/anime-list-master.xml").content)
+Manami = requests.get("https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database.json").json()
+
+anime_dicts = {}
+
+for anime in AniDBIDs.xpath("//anime"):
+    anidb_id = str(anime.xpath("@anidbid")[0])
+    if not anidb_id:
+        continue
+    anidb_id = int(anidb_id[1:]) if anidb_id[0] == "a" else int(anidb_id)
+    anime_dict = {}
+    tvdb_id = str(anime.xpath("@tvdbid")[0])
+    try:
+        if tvdb_id:
+            anime_dict["tvdb_id"] = int(tvdb_id)
+    except ValueError:
+        pass
+    imdb_id = str(anime.xpath("@imdbid")[0])
+    if imdb_id.startswith("tt"):
+        anime_dict["imdb_id"] = imdb_id
+    anime_dicts[anidb_id] = anime_dict
+
+for anime in Manami["data"]:
+    if "sources" not in anime:
+        continue
+
+    anidb_id = None
+    mal_id = None
+    anilist_id = None
+    for source in anime["sources"]:
+        if "anidb.net" in source:
+            anidb_id = int(source.partition("anime/")[2])
+        elif "myanimelist" in source:
+            mal_id = int((source.partition("anime/")[2]))
+        elif "anilist.co" in source:
+            anilist_id = int((source.partition("anime/")[2]))
+    if anidb_id and anidb_id in anime_dicts:
+        if mal_id:
+            anime_dicts[anidb_id]["mal_id"] = mal_id
+        if anilist_id:
+            anime_dicts[anidb_id]["anilist_id"] = anilist_id
+
+with open("pmm_edits.json", "r") as f:
+    for anidb_id, ids in json.load(f).items():
+        anidb_id = int(anidb_id)
+        if anidb_id in anime_dicts:
+            if "tvdb_id" in ids:
+                anime_dicts[anidb_id]["tvdb_id"] = ids["tvdb_id"]
+            if "mal_id" in ids:
+                anime_dicts[anidb_id]["mal_id"] = ids["mal_id"]
+            if "anilist_id" in ids:
+                anime_dicts[anidb_id]["anilist_id"] = ids["anilist_id"]
+            if "imdb_id" in ids:
+                anime_dicts[anidb_id]["imdb_id"] = ids["imdb_id"]
+
+with open("pmm_anime_ids.json", "w") as write:
+    json.dump(anime_dicts, write, indent=2)
+
+with open("README.md", "r") as f:
+    data = f.readlines()
+
+data[1] = f"Last generated at: {datetime.utcnow().strftime('%B %d, %Y %I:%M %p')}\n"
+
+with open("README.md", "w") as f:
+    f.writelines(data)
